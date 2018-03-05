5c870efad227357169cbdd2c5805040c5e054456
Manny Isles <mannysoft@gmail.com>
2018-03-05 14:44:01 +0800

    first

---
+++ tests/SSSTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace IslesTech\Tests;
+use PHPUnit\Framework\TestCase;
+
+class SSSTest extends TestCase
+{
+
+    public function testSampleTest()
+    {
+        $this->assertSame(1, 1);
+    }
+
+    
+}
